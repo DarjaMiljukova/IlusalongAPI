933dad08b612e38f9ef675ccb2d2f6ef2d871e32
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2025-04-29 20:53:34 +0300

    add render.yaml

--- obj/Debug/net7.0/IlusalongAPI.AssemblyInfo.cs
+++ obj/Debug/net7.0/IlusalongAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IlusalongAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5013cc6fc75e1267230b4ac03809bb4285f3fee6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a87121f944a72cf23ce8ca3a4c1d39b266b93c")]
 [assembly: System.Reflection.AssemblyProductAttribute("IlusalongAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IlusalongAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
